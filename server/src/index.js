078445e22f77145b45cb7a51af3e11d7bdb3274c
Shavidu Keshan <shavidu4321@gmail.com>
2024-10-13 22:50:46 +0530

    recrete user route file

--- server/src/index.js
+++ server/src/index.js
@@ -1,31 +1,23 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import { userRouter } from "./routes/users.js"; // Correct import statement
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const MONGO_URI = "mongodb+srv://shavidu4321:Recipe123App@cluster0.tfsvv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+app.use("/auth", userRouter);
 
 // Connect to MongoDB
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect("mongodb+srv://shavidu4321:Recipe123App@cluster0.tfsvv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    .then(() => console.log("Connected to MongoDB successfully!"))
+    .catch(err => console.error("MongoDB connection error:", err));
 
-// Connection success and error handling
-mongoose.connection.on("connected", () => {
-  console.log("Connected to MongoDB successfully!");
-});
+app.use("/api/users", userRouter); // Use the user router
 
-mongoose.connection.on("error", (err) => {
-  console.error("MongoDB connection error:", err);
-});
-
-// Start the server
 app.listen(3001, () => {
-  console.log("Server Started!");
+    console.log("Server Started!");
 });
 
 
--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,6 +1,6 @@
 import express from "express";
-import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
