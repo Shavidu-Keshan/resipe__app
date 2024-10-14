93df48f16618622ccf48ddeac1cbd04e15fba3da
Shavidu Keshan <shavidu4321@gmail.com>
2024-10-14 16:24:11 +0530

    create user register and login part in ackend

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,7 +1,46 @@
 import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { UserModel } from "../models/Users.js";
 
 const router = express.Router();
+
+router.post("/register",async (req, res) => {
+    const {username, password} = req.body;
+
+    const user =  await UserModel.findOne({username});
+
+    if(user) {
+        return res.json({message: "User already exists"});
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new UserModel({username, password: hashedPassword});
+    await newUser.save();
+
+    res.json({message: "User registered successfully"});
+
+    res.json(user);
+});
+
+router.post("/login", async (req, res) => {
+    const {username, password} = req.body;
+
+    const user = await UserModel.findOne({username});
+
+    if(!user) {
+        return res.json({message: "User not found"});
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if(!isPasswordValid) {
+        return res.json({message: "username or password is not valid"});
+    }
+
+    const token = jwt.sign({id: user._id}, "secret");
+    res.json({token, userID: user._id});
+})
 
 export{router as userRouter};
